3c048b518623ebca1d288d26ff241c0f477380ad
TIMB <timb@cpan.org>
2016-03-19 23:24:00 +0000

    Import https://cpan.metacpan.org/authors/id/T/TI/TIMB/Devel-NYTProf-6.02_91.tar.gz

--- lib/Devel/NYTProf/Core.pm
+++ lib/Devel/NYTProf/Core.pm
@@ -12,7 +12,7 @@ package Devel::NYTProf::Core;
 
 use XSLoader;
 
-our $VERSION = '6.02_90';    # increment with XS changes too
+our $VERSION = '6.02_91';    # increment with XS changes too
 
 XSLoader::load('Devel::NYTProf', $VERSION);
 
